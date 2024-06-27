7cc944ccc59d88d8aa31e35e7d10baf84b67a30a
Alexis Guzman <spartanthelexvro@gmail.com>
2024-06-27 11:21:02 -0600

    Soporte para key como String's

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ import hastTable.HashTable;
 public class Main {
     public static void main(String[] args) {
 
-        HashTable table = new HashTable(13);
-        table.add(1, "Hola");
-        table.add(1, "Hola2");
-        table.getAt(1);
+        HashTable table = new HashTable();
+        table.add("Primero", "Hola");
+        table.add("Primero", "Hola2");
+        table.add("Segundo", "Chanchito feliz");
+        table.add("Alexis", 1);
+        table.getAt("Primero");
+        table.getAt("Segundo");
+        table.getAt("Alexis");
+
     }
 }
--- hastTable/HashTable.java
+++ hastTable/HashTable.java
@@ -4,33 +4,47 @@ import linkedList.LinkedList;
 
 public class HashTable {
     private LinkedList[] table;
-    private LinkedList list;
+    private int size = 13;
 
-
-    public HashTable(int size) {
-        this.table = new LinkedList[size];
-        for (int i = 0; i < this.table.length; i++) {
+    public HashTable() {
+        this.table = new LinkedList[this.size];
+        for (int i = 0; i < this.size; i++) {
             this.table[i]= new LinkedList();
         }
     }
 
+    //Función para la coneverión a ASCCI
+    private int converTo(String text){
+        text.split("");
+        int result = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char character = text.charAt(i);     //Obtengo los caracteres de la cadena
+            int asciiValue = (int) character;    //Se convierten a números enteros ASCCI
+            result += asciiValue;                //El resultado de la suma se guarda para mandarlo al hash
+        }
+        return result;
+    }
+
     //Función hash por el método de la división
     private int hashCodeD(int value) {
-        int hash = ((value & 0x7fffffff)% this.table.length);
+        int hash = ((value & 0x7fffffff)% this.size);
         return hash;
     }
 
     //Función hash por el método de la multiplicaión
-    private int hasCodeM(int value) {
+    private int hashCodeM(int value) {
         final double A = (Math.sqrt(5)-1)/2; //Constante
-        double hash = (this.table.length*(value*A % 1));
+        double hash = (this.size*(value*A % 1));
         return (int) hash; //Conversión a su parte entera
     }
 
     //Agregar
-   public void add(int value, Object data) {
-        if (value >= 0) {
-            int hash = hashCodeD(value);
+   public void add(String key, Object data) {
+        int newKey = this.converTo(key);
+
+        if (newKey >= 0) {
+            int hash = hashCodeM(newKey);
             this.table[hash].push(data);
         } else {
             System.out.println("Error al agregar");
@@ -38,14 +52,16 @@ public class HashTable {
    }
 
    //Obtener los datos
-    public void getAt(int value) {
-        if (value >= 0) {
-            int hash = hashCodeD(value);
+    public void getAt(String key) {
+        int newKey = this.converTo(key);
+
+        if (newKey >= 0) {
+            int hash = hashCodeM(newKey);
             for (int i = 0; i < this.table[hash].size(); i++){
                 System.out.println(this.table[hash].getAt(i));
             }
         } else {
-            System.out.println("Error al agregar");
+            System.out.println("Error al obtener");
         }
     }
 }
